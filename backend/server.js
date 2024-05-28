c68163a19c7dbcb89665362503126595ccc78dda
musoye <oyebamijimustapha44@gmail.com>
2024-05-28 23:49:21 +0100

    Feat: Projrct Starting

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.json({ 'status': 'success', 'message': 'Welcome to the Ngreen API' })
+})
+app.get('/status', (req, res) => {
+    res.json({ 'status': 'success' })
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({ status: 'Something broke!' });
+});
+
+app.listen(port, () => {
+    console.log('welcome to NGREEN, YOur ultimate Rider server')
+})
